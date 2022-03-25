08d7530d7fff33354c92170b0a9a777dda9f0b68
Andrew Kemper <andrewkemper1@gmail.com>
2022-03-25 12:27:41 -0400

    Adding async command example

--- ScoreKeeper/ViewModels/MainPageViewModel.cs
+++ ScoreKeeper/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace ScoreKeeper.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+
+        public AsyncCommand NewGameCommand {get; set;}
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+            _navigationService = navigationService;
+
             Title = "Main Page";
+
+            NewGameCommand = new AsyncCommand(ExecuteNewGame, allowsMultipleExecutions: false);
+        }
+
+        private async Task ExecuteNewGame()
+        {
+            await _navigationService.NavigateAsync("NewGamePage");
         }
     }
 }
