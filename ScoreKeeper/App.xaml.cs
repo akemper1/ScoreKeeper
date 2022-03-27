4c697049caca06f7b5e1ddb7ce3e3f47c2d31444
Andrew Kemper <andrewkemper1@gmail.com>
2022-03-27 16:14:02 -0400

    Adding fonts

--- ScoreKeeper/App.xaml.cs
+++ ScoreKeeper/App.xaml.cs
@@ -7,6 +7,18 @@ using Xamarin.Essentials.Implementation;
 using Xamarin.Essentials.Interfaces;
 using Xamarin.Forms;
 
+[assembly: ExportFont("FontAwesome-Regular.ttf", Alias = "FontAwesome_Regular")]
+[assembly: ExportFont("FontAwesome-Solid.ttf", Alias = "FontAwesome_Solid")]
+
+[assembly: ExportFont("Mulish-Black.ttf", Alias = "Mulish_Black")]
+[assembly: ExportFont("Mulish-Bold.ttf", Alias = "Mulish_Bold")]
+[assembly: ExportFont("Mulish-ExtraBold.ttf", Alias = "Mulish_ExtraBold")]
+[assembly: ExportFont("Mulish-ExtraLight.ttf", Alias = "Mulish_ExtraLight")]
+[assembly: ExportFont("Mulish-Light.ttf", Alias = "Mulish_Light")]
+[assembly: ExportFont("Mulish-Medium.ttf", Alias = "Mulish_Medium")]
+[assembly: ExportFont("Mulish-Regular.ttf", Alias = "Mulish_Regular")]
+[assembly: ExportFont("Mulish-SemiBold.ttf", Alias = "Mulish_SemiBold")]
+
 namespace ScoreKeeper
 {
     public partial class App
